81b9c2aff8ae0cff772183c0dad4dd4740ba4d05
tcoops <345243921@qq.com>
2016-09-04 09:57:09 +0800

    update

--- hihocoder/hihocoder.cpp
+++ hihocoder/hihocoder.cpp
@@ -1,13 +1,70 @@
-#include <cstdio>
-#include <iostream>
-using namespace std;
+//AC
 
-int main() {
-  int a, b;
-  cin >> a >> b;
-  cout << a + b << endl;
-  return 0;
+// Author: RejudgeX
+// Level -> CF/TC -> Yellow
+// > -> Ag
+// -> F/L/A/G
+// -> Latency 「2017/5/15」
+
+//Google mock-contest 1
+//RejudgeX: 2016/8/3 22:35 - 24:00
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <map>
+#include <ctime>
+//#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define per(i,a,n) for (int i=n-1;i>=a;i--)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define SZ(x) ((int)(x).size())
+#define fi first
+#define se second
+typedef vector<int> VI;
+typedef long long ll;
+typedef pair<int,int> PII;
+const ll MOD = 1000000007;
+ll powmod(ll a,ll b) {ll res=1;a%=MOD;for(;b;b>>=1){if(b&1)res=res*a%MOD;a=a*a%MOD;}return res;}
+// head
+
+inline void gn(long long &x){
+    int sg=1;
+    char c;while(((c=getchar())<'0'||c>'9')&&c!='-');c=='-'?(sg=-1,x=0):(x=c-'0');
+    while((c=getchar())>='0'&&c<='9')x=x*10+c-'0';x*=sg;
 }
 
-1000
-ok
+inline void gn(int&x){long long t;gn(t);x=t;}
+inline void gn(unsigned long long&x){long long t;gn(t);x=t;}
+inline void gn(double&x){double t;scanf("%lf",&t);x=t;}
+inline void gn(long double&x){double t;scanf("%lf",&t);x=t;}
+
+const int N = 5000010;
+bool isPrime[1000000000];
+int prime[N];
+
+void pre() {
+  int cnt = 0;
+  memset(isPrime, true, sizeof(isPrime));
+  for(int i = 2; i < 1e9; ++i) {
+    if(isPrime[i]) {
+      prime[cnt++] = i;
+    }
+    for(int j = 0; j < cnt && i * prime[j] < 1e9; ++j) {
+      isPrime[j] = false;
+      if(i % prime[j] == 0) break;
+    }
+  }
+}
+
+int main() {
+  pre();
+}

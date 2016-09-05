8f7d109638414dda486bec2938f691c425a9bf45
tcoops <345243921@qq.com>
2016-09-05 22:14:06 +0800

    update

--- dp-train/dp-train.cpp
+++ dp-train/dp-train.cpp
@@ -52,11 +52,12 @@ inline void gn(long double&x){double t;scanf("%lf",&t);x=t;}
 const int N = 1010;
 const int INF = 0x3f3f3f3f;
 int dp[N][2];
+bool shadow[N];
 
 struct platform {
   int l, r, h;
   bool operator <(const platform &rhs) const {
-    returnr h > rhs.h;
+    return h > rhs.h;
   }
 } p[N];
 
@@ -77,29 +78,34 @@ int main() {
     sort(p, p + cnt);
 
     memset(dp, INF, sizeof(dp));
+    memset(shadow, false, sizeof(shadow));
     dp[0][0] = dp[0][1] = 0;
     for(int i = 1; i < cnt; ++i) {
       for(int j = 0; j < i; ++j) {
+        if(shadow[j]) continue;
+        if((p[j].l > p[i].l && p[j].l < p[i].r) || (p[j].r > p[i].l && p[j].r < p[i].r)) {
+          shadow[j] = true;
+        }
         if(p[j].h - p[i].h <= lim) {
           if(p[j].l > p[i].l && p[j].l < p[i].r) {
             dp[i][0] = min(dp[i][0], dp[j][0] + p[j].h - p[i].h + p[j].l - p[i].l);
-            dp[i][1] = min(dp[i][0], dp[j][0] + p[j].h - p[i].h + p[i].r - p[j].l);
+            dp[i][1] = min(dp[i][1], dp[j][0] + p[j].h - p[i].h + p[i].r - p[j].l);
           }
           if(p[j].r > p[i].l && p[j].r < p[i].r) {
-            dp[i][0] = min(dp[i][0], dp[j][0] + p[j].h - p[i].h + p[j].r - p[i].l);
-            dp[i][1] = min(dp[i][0], dp[j][0] + p[j].h - p[i].h + p[i].r - p[j].r);
+            dp[i][0] = min(dp[i][0], dp[j][1] + p[j].h - p[i].h + p[j].r - p[i].l);
+            dp[i][1] = min(dp[i][1], dp[j][1] + p[j].h - p[i].h + p[i].r - p[j].r);
           }
         }
       }
     }
 
     int ans = INF;
-    for(int i = cnt - 1; i >= 0 && p[i].h <= lim; ++i) {
-      if(dp[i][0] < ans) {
-        ans = dp[i][0];
+    for(int i = cnt - 1; i >= 0 && p[i].h <= lim; --i) {
+      if(dp[i][0] + p[i].h < ans && !shadow[i]) {
+        ans = dp[i][0] + p[i].h;
       }
-      if(dp[i][1] < ans) {
-        ans = dp[i][1];
+      if(dp[i][1] + p[i].h < ans && !shadow[i]) {
+        ans = dp[i][1] + p[i].h;
       }
     }
     cout << ans << endl;
